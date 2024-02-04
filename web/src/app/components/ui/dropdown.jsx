8b63e6e63c200bfc3e3357557b0ef661d02a445a
bhigoreduardo <bhigoreduardo@gmail.com>
2024-02-03 23:03:30 -0400

    :lipstick: Refactor: Style dropdown menu

--- web/src/app/components/ui/dropdown.jsx
+++ web/src/app/components/ui/dropdown.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
 import { CaretRight } from 'phosphor-react'
 
 import { categories } from '../../../utils/mock'
+import { makeArrTree } from '../../../utils/format'
 import NavItem from './common/nav-item'
 import BannerCard from './cards/banner-card'
 import RelatedCard from './cards/related-card'
 
-const Categories = ({ setOpenDropdown, isNonCategories = false }) =>
+const Categories = ({
+  categories,
+  setOpenDropdown,
+  setOpenChildren,
+  isNonCategories = false,
+}) =>
   categories?.map((item, i) => (
     <NavItem
       key={i}
@@ -23,42 +30,66 @@ const Categories = ({ setOpenDropdown, isNonCategories = false }) =>
       }
       isSidebar
       label={item.name}
+      onMouseOver={() => setOpenChildren && setOpenChildren(i)}
       className="group hover:bg-gray-50 xl:w-[240px] sm:w-[200px]"
     />
   ))
 
 export default function Dropdown({ setOpenDropdown }) {
+  const [openChildren, setOpenChildren] = useState(null)
+  const categoriesTree = categories ? makeArrTree(categories, null) : []
+
   return (
     <aside className="absolute left-2 lg:left-6 top-[calc(100%+10px)] flex gap-3 duration-300 ease-in-out z-40">
       {/* CATEGORIES */}
       <nav className="flex flex-col border border-gray-100 bg-white rounded-sm shadow-md py-2 h-fit max-w-[300px] sm:w-auto w-[250px]">
-        <Categories setOpenDropdown={setOpenDropdown} />
+        <Categories
+          categories={categoriesTree}
+          setOpenDropdown={setOpenDropdown}
+          setOpenChildren={setOpenChildren}
+        />
       </nav>
       {/* SUB CATEGORIES */}
-      <nav className="hidden sm:flex md:flex-row flex-col gap-5 justify-between border border-gray-100 bg-white rounded-sm shadow-md p-2">
-        <div className="flex flex-col">
-          <Categories isNonCategories setOpenDropdown={setOpenDropdown} />
-        </div>
-        <div className="flex gap-5 lg:flex-row flex-col">
-          <div className="flex flex-col gap-4">
-            <h4 className="font-semibold text-gray-900 text-base uppercase mb-5">
-              Lorem Ipsum is simply
-            </h4>
-            {Array.from({ length: 3 }).map((_, i) => (
-              <RelatedCard key={i} className="max-w-[300px]" />
-            ))}
-          </div>
-          <div className="max-w-[300px]">
-            <BannerCard
-              title="Sofá Reclinável Minas"
-              description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
-              offer={299.99}
-              image="product/product-1-cover.png"
-              smallBanner
-            />
-          </div>
-        </div>
-      </nav>
+      {categoriesTree.map(
+        (item, i) =>
+          item.children?.length > 0 &&
+          openChildren === i && (
+            <nav
+              key={i}
+              className="hidden sm:flex md:flex-row flex-col gap-5 justify-between border border-gray-100 bg-white rounded-sm shadow-md p-2"
+            >
+              <div className="flex flex-col">
+                <Categories
+                  categories={item.children.map((value) => ({
+                    ...value,
+                    slug: `${item.slug},${value.slug}`,
+                  }))}
+                  isNonCategories
+                  setOpenDropdown={setOpenDropdown}
+                />
+              </div>
+              <div className="flex gap-5 lg:flex-row flex-col">
+                <div className="flex flex-col gap-4">
+                  <h4 className="font-semibold text-gray-900 text-base uppercase mb-5">
+                    Lorem Ipsum is simply
+                  </h4>
+                  {Array.from({ length: 3 }).map((_, i) => (
+                    <RelatedCard key={i} className="max-w-[300px]" />
+                  ))}
+                </div>
+                <div className="max-w-[300px]">
+                  <BannerCard
+                    title="Sofá Reclinável Minas"
+                    description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+                    offer={299.99}
+                    image="product/product-1-cover.png"
+                    smallBanner
+                  />
+                </div>
+              </div>
+            </nav>
+          )
+      )}
     </aside>
   )
 }
